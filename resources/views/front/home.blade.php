3730260538812644d43a8d2e07cf0d860d15a274
Rommel Cuneta <cunetarommel0620@gmail.com>
2020-03-29 17:32:26 +0800

    setup

---
+++ resources/views/front/home.blade.php
@@ -0,0 +1,4 @@
+@extends("layout")
+@section('content')
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae aut veniam placeat. Reiciendis voluptas amet, perferendis ab, molestiae architecto, delectus unde nobis aspernatur necessitatibus repellendus. Obcaecati dicta necessitatibus modi libero.
+@endsection
