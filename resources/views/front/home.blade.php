b2cc05bbd365dd8313ba5838da2480495f7ada5d
Rommel Cuneta <cunetarommel0620@gmail.com>
2020-04-03 15:09:33 +0800

    homepage

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -1,5 +1,6 @@
 @extends("layout")
 @section('content')
+    <!-- HERO SECTION -->
     <section class="hero-section">
         <div class="categories">
             <div class="categories__label">Categories</div>
@@ -9,30 +10,12 @@
                         <span class="text">Construction</span>
                         <span class="mdi mdi-chevron-right"></span>
                     </a>
-                    <ul class="sub-category">
-                        @for ($i = 0; $i < 10; $i++)
-                            <li class="sub-category__item">
-                                <a class="sub-categories__item-link" href="javascript:">
-                                    <span class="text">Lorem ipsum</span>
-                                </a>
-                            </li>
-                        @endfor
-                    </ul>
                 </li>
                 <li class="categories__item">
                     <a class="categories__item-link" href="javascript:">
                         <span class="text">Automotive</span>
                         <span class="mdi mdi-chevron-right"></span>
                     </a>
-                    {{-- <ul class="sub-category">
-                        @for ($i = 0; $i < 10; $i++)
-                            <li class="sub-category__item">
-                                <a class="sub-categories__item-link" href="javascript:">
-                                    <span class="text">Lorem ipsum</span>
-                                </a>
-                            </li>
-                        @endfor
-                    </ul> --}}
                 </li>
                 <li class="categories__item">
                     <a class="categories__item-link" href="javascript:">
@@ -94,6 +77,15 @@
                         <span class="mdi mdi-chevron-right"></span>
                     </a>
                 </li>
+                <ul class="sub-category">
+                    @for ($i = 0; $i < 10; $i++)
+                        <li class="sub-category__item">
+                            <a class="sub-categories__item-link" href="javascript:">
+                                <span class="text">Lorem ipsum</span>
+                            </a>
+                        </li>
+                    @endfor
+                </ul>
             </ul>
         </div>
         <div class="product-slider">
@@ -136,23 +128,112 @@
             </div>
         </div>
     </section>
+    <!-- END HERO SECTION -->
+
+    <!-- ADS SECTION -->
     <section class="ads-section">
         @for ($i = 0; $i < 2; $i++)
-            <div class="ads">
-                <span>zphen ads</span>
-            </div>
+        <div class="ads">
+            <span>zphen ads</span>
+        </div>
         @endfor
     </section>
+    <!-- END ADS SECTION -->
+
+    <!-- ACCREDITATION SECTION -->
     <section class="accreditation">
         <div class="label">Available slots for free Accreditation</div>
         <button class="btn-inquire">INQUIRE</button>
     </section>
+    <!-- END ACCREDITATION SECTION -->
+
+    <!-- FEATURE PRODUCTS SECTION -->
     <section class="feature-products">
         <header class="section-header">
             <div class="section-title">FEATURED PRODUCTS</div>
             <a class="section-action" href="javascript:">See all <span class="mdi mdi-chevron-right"></span></a>
         </header>
+        <div class="product-wrapper">
+            @for ($i = 0; $i < 5; $i++)
+                <div class="product-item">
+                    <div class="product-item__top">
+                        <div class="product-item__image">
+                            <img src="https://static.bhphoto.com/images/images500x500/1484050840_1308819.jpg" alt="">
+                        </div>
+                    </div>
+                    <div class="product-item__bottom">
+                        <div class="product-item__info">
+                            <div class="product-item__name">Nikon D5600</div>
+                            <div class="product-item__desc">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas, minus.</div>
+                        </div>
+                        <div class="product-item__order">
+                            <div class="product-item__price">
+                                <div class="product-item__current">₱ 24,000.00</div>
+                                <div class="product-item__prev">₱ 42,000.00</div>
+                            </div>
+                            <div class="product-item__action">
+                                <button class="z-btn-outline">Add to cart</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endfor
+        </div>
     </section>
+    <!-- END FEATURE PRODUCTS SECTION -->
+    
+    <!-- MOST POPULAR SECTION -->
+    <section class="most-popular">
+        <header class="section-header">
+            <div class="section-title">MOST POPULAR</div>
+            <a class="section-action" href="javascript:">See all <span class="mdi mdi-chevron-right"></span></a>
+        </header>
+        <div class="product-wrapper">
+            @for ($i = 0; $i < 5; $i++)
+                <div class="product-item">
+                    <div class="product-item__top">
+                        <div class="product-item__image">
+                            <img src="https://static.bhphoto.com/images/images500x500/1484050840_1308819.jpg" alt="">
+                        </div>
+                    </div>
+                    <div class="product-item__bottom">
+                        <div class="product-item__info">
+                            <div class="product-item__name">Nikon D5600</div>
+                            <div class="product-item__desc">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas, minus.</div>
+                        </div>
+                        <div class="product-item__order">
+                            <div class="product-item__price">
+                                <div class="product-item__current">₱ 24,000.00</div>
+                                <div class="product-item__prev">₱ 42,000.00</div>
+                            </div>
+                            <div class="product-item__action">
+                                <button class="z-btn-outline">Add to cart</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endfor
+        </div>
+        <div class="d-flex justify-content-center py-3 mt-4">
+            <button class="z-btn-filled">LOAD MORE</button>
+        </div>
+    </section>
+    <!-- END MOST POPULAR SECTION -->
+    
+    <!-- BUILD, RENOVATE, PROJECTS SECTION -->
+    <section class="grid">
+        <div class="grid__item">
+            <div class="label">Build</div>
+        </div>
+        <div class="grid__item">
+            <div class="label">Renovate</div>
+        </div>
+        <div class="grid__item">
+            <div class="label">Projects</div>
+        </div>
+    </section>
+    <!-- END BUILD, RENOVATE, PROJECTS SECTION -->
+
 @endsection
 
 @section('style')
